e68a43af5bfbf6318cc91cfff9b8431c0081b7b7
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-08-06 20:51:41 -0300

    autenticação Spring security

---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.usuario;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Usuario {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
