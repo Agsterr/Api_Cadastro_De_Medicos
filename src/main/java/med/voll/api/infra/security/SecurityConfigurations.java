1f7fa6a26eae66b8ed153ac976ab25b406390eb0
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-08-06 21:40:56 -0300

    implementação classe security

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,9 +2,13 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -19,6 +23,21 @@ public class SecurityConfigurations {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+
+    //assinatura para ensinar o spring a injetar algumas dependencias
+     @Bean
+    //metodo para injetar a dependencia da classe AuthenticationConfiguration
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+    //para o spring reconhecer como estou criptografando minhas senhas
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+      return new BCryptPasswordEncoder() ;
     }
 
 }
