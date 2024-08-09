4c358eed795a90e285cfaa49561e8078ebec43e4
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-08-08 21:21:04 -0300

    senha com token e variavel de ambiente

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,20 +4,43 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.ApiApplication;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
 
 @Service
 public class TokenService {
 
-    public String gerarToken(){
+
+
+    //reconhecer a variavel que eu criei no application.properties
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String gerarToken(Usuario usuario) {
         try {
-           var algoritimo = Algorithm.HMAC256("12345678");
-            String token = JWT.create()
-                    .withIssuer("auth0")
+            var algoritimo = Algorithm.HMAC256(secret);
+          return JWT.create()
+                    .withIssuer("API Voll.med")
+                  .withSubject(usuario.getLogin())
+                  .withClaim("id",usuario.getId())
+                  .withExpiresAt(dataExpiracao())
                     .sign(algoritimo);
-        } catch (JWTCreationException exception){
-            // Invalid Signing configuration / Couldn't convert Claims.
+        } catch (JWTCreationException exception) {
+             throw new RuntimeException("erro ao gerar token jwt", exception);
         }
+
+    }
+
+    private Instant dataExpiracao() {
+
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
 
     }
 
