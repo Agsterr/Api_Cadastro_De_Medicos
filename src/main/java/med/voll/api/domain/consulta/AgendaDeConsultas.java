0720055aa17ecc2776562d3c123f51c16a775646
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-08-12 21:55:28 -0300

    query

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -33,7 +33,7 @@ public class AgendaDeConsultas {
         }
 
         var medico = escolherMedico(dados);
-        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
 
 
         var consulta = new Consulta(null,medico, paciente,dados.data() );
@@ -45,6 +45,17 @@ public class AgendaDeConsultas {
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+     if (dados.idMedico() != null){
+         return medicoRepository.getReferenceById(dados.idMedico());
+     }
+
+     if (dados.especialidade() == null){
+         throw new ValidacaoException("Epecialidade é obrigatória quando médico não for escolhido!");
+     }
+
+     return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(),dados.data());
+
+
 
 
 
